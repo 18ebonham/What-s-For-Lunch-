4cac192be8592bd3929d6292bbcbe429a6083545
Hartley Bonham <hartleybonham@hartleys-macbook-pro.local>
2018-03-14 11:31:38 -0700

    updated

--- Lunch Menu/TodayViewController.swift
+++ Lunch Menu/TodayViewController.swift
@@ -15,11 +15,19 @@ class TodayViewController: UIViewController {
     var grillLike = false
     var entreeLike = false
     var dessertLike = false
-
     @IBOutlet weak var todayLunchWebView: WKWebView!
     
-    @IBOutlet weak var todayDateLabel: UILabel!
+    @IBOutlet weak var soupLabel: UILabel!
+    @IBOutlet weak var grillLabel: UILabel!
+    @IBOutlet weak var entreeLabel: UILabel!
+    @IBOutlet weak var dessertLabel: UILabel!
     
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var cafeClosedLabel: UILabel!
+
+    @IBOutlet weak var todayDateLabel: UILabel!
+    var weekMenuDict = [String:[String]]()
+    var dayOfWeek = [1:"SUN",2:"MON",3:"TUE",4:"WED",5:"THU",6:"FRI",7:"SAT"]
 
     
     override func viewDidLoad() {
@@ -30,16 +38,91 @@ class TodayViewController: UIViewController {
         todayDateLabel.text = "............. 🍴\(DateFormatter.localizedString(from: NSDate() as Date, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.none))🍴 ............."
 
         // Do any additional setup after loading the view.
+        // TODO find day of week
+        var dow = Calendar.current.component(.weekday, from: Date())
+        print(">>>Day of week:<<<<")
+        print(dayOfWeek[dow])
+        //dow=7
+        if (dow == 1 || dow == 7) {
+            cafeClosedLabel.text = "Café is closed today. \nShowing menu for Monday"
+            dow = 2
+        }
         
-        
-        
-        //display today's lunch
-        //let url = URL(string: "https://docs.google.com/document/d/1r589OZCs3wh2_qZ74ijOHV76Ov4CSbCuSJDQ4Lp3em4/edit")
-        
-        //todayLunchWebView.load(URLRequest(url: url!))
+        parseMenuHtml()
+        let todaysMenu = weekMenuDict[dayOfWeek[dow]!]
+        soupLabel.text = todaysMenu![0]
+        grillLabel.text = todaysMenu![1]
+        entreeLabel.text = todaysMenu![2]
+        dessertLabel.text = todaysMenu![3]
         
     }
     
+    func parseMenuHtml() {
+        let url = URL(string: "https://www.castilleja.org/page.cfm?p=940953")
+        let urlString = try! String(contentsOf: url!, encoding: .utf8)
+        
+        var htmlContentMon = urlString as NSString
+        var htmlContentTue = urlString as NSString
+        var htmlContentWed = urlString as NSString
+        var htmlContentThu = urlString as NSString
+        var htmlContentFri = urlString as NSString
+        
+        //Monday
+        var startMon = htmlContentMon.range(of: "Mon,").location
+        htmlContentMon = htmlContentMon.substring(from: startMon) as NSString
+        startMon = htmlContentMon.range(of: "Soup:").location
+        htmlContentMon = htmlContentMon.substring(from: startMon) as NSString
+        let endMon = htmlContentMon.range(of: "</p>").location
+        let foodsForMonday = htmlContentMon.substring(to: endMon)
+        let mondayLunch = foodsForMonday.components(separatedBy: "<br />")
+        weekMenuDict["MON"] = mondayLunch
+
+        
+        //Tuesday
+        var startTue = htmlContentTue.range(of: "Tues,").location
+        htmlContentTue = htmlContentTue.substring(from: startTue) as NSString
+        startTue = htmlContentTue.range(of: "Soup:").location
+        htmlContentTue = htmlContentTue.substring(from: startTue) as NSString
+        let endTue = htmlContentTue.range(of: "</p>").location
+        let foodsForTuesday = htmlContentTue.substring(to: endTue)
+        let tuesdayLunch = foodsForTuesday.components(separatedBy: "<br />")
+        weekMenuDict["TUE"] = tuesdayLunch
+
+        
+        //Wednesday
+        var startWed = htmlContentWed.range(of: "Wed,&nbsp").location
+        htmlContentWed = htmlContentWed.substring(from: startWed) as NSString
+        startWed = htmlContentWed.range(of: "Soup:").location
+        htmlContentWed = htmlContentWed.substring(from: startWed) as NSString
+        let endWed = htmlContentWed.range(of: "</p>").location
+        let foodsForWednesday = htmlContentWed.substring(to: endWed)
+        let wednesdayLunch = foodsForWednesday.components(separatedBy: "<br />")
+        weekMenuDict["WED"] = wednesdayLunch
+ 
+        
+        //Thursday
+        var startThu = htmlContentThu.range(of: "Thurs,").location
+        htmlContentThu = htmlContentThu.substring(from: startThu) as NSString
+        startThu = htmlContentThu.range(of: "Soup:").location
+        htmlContentThu = htmlContentThu.substring(from: startThu) as NSString
+        let endThu = htmlContentThu.range(of: "</td>").location
+        let foodsForThursday = htmlContentThu.substring(to: endThu)
+        let thursdayLunch = foodsForThursday.components(separatedBy: "<br />")
+        weekMenuDict["THU"] = thursdayLunch
+
+        
+        //Friday
+        var startFri = htmlContentFri.range(of: "Fri,&nbsp").location
+        htmlContentFri = htmlContentFri.substring(from: startFri) as NSString
+        startFri = htmlContentFri.range(of: "Soup:").location
+        htmlContentFri = htmlContentFri.substring(from: startFri) as NSString
+        let endFri = htmlContentFri.range(of: "</td>").location
+        let foodsForFriday = htmlContentFri.substring(to: endFri)
+        let fridayLunch = foodsForFriday.components(separatedBy: "<br />")
+        weekMenuDict["FRI"] = fridayLunch
+
+        
+    }
     // soup like button
     @IBAction func soupLikeButton(_ sender: UIButton) {
         //change button to filled in version
@@ -90,6 +173,7 @@ class TodayViewController: UIViewController {
     
     //submit button
     @IBAction func submitButton(_ sender: Any) {
+        print("Submitting repponse")
         var urlSoFar = "https://docs.google.com/forms/d/e/1FAIpQLScbi6oEupJMEStLrzynW5n4yAKun_wDf63J3lDC6h1cXy2YAg/formResponse?ifq&"
         if soupLike {
             urlSoFar += "entry.1500086948=Soup&"
@@ -108,10 +192,18 @@ class TodayViewController: UIViewController {
         let urlString = try! String(contentsOf: url!, encoding: .utf8)
         
         //submit button is now unclickable and changes text
+        submitButton.setTitle("Thank you for the feedback !", for: .normal)
+        submitButton.isEnabled = false
+        
+        
+        //make button re-enable at midnight
+        //???????
+        //???????
+        //???????
+        
         
     }
     
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- Lunch Menu/WebsiteLunchViewController.swift
+++ Lunch Menu/WebsiteLunchViewController.swift
@@ -11,15 +11,94 @@ import WebKit
 
 class WebsiteLunchViewController: UIViewController {
 
+    @IBOutlet weak var mondayFoodLabel: UILabel!
+    @IBOutlet weak var tuesdayFoodLabel: UILabel!
+    @IBOutlet weak var wednesdayFoodlabel: UILabel!
+    @IBOutlet weak var thursdayFoodLabel: UILabel!
+    @IBOutlet weak var fridayFoodLabel: UILabel!
+    
     @IBOutlet weak var websiteLunchView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.castilleja.org/page.cfm?p=940953")
+        let urlString = try! String(contentsOf: url!, encoding: .utf8)
         
-        let url = URL(string: "https://docs.google.com/document/d/1MUFWGAU7VrNpaaX7aoJotm1ae_kz_oPTiKVIaEPAhzc/edit")
-        websiteLunchView.loadRequest(URLRequest(url: url!))
+        var htmlContentMon = urlString as NSString
+        var htmlContentTue = urlString as NSString
+        var htmlContentWed = urlString as NSString
+        var htmlContentThu = urlString as NSString
+        var htmlContentFri = urlString as NSString
+        
+        //Monday
+        var startMon = htmlContentMon.range(of: "Mon,").location
+        htmlContentMon = htmlContentMon.substring(from: startMon) as NSString
+        startMon = htmlContentMon.range(of: "Soup:").location
+        htmlContentMon = htmlContentMon.substring(from: startMon) as NSString
+        let endMon = htmlContentMon.range(of: "</p>").location
+        let foodsForMonday = htmlContentMon.substring(to: endMon)
+        let mondayLunch = foodsForMonday.components(separatedBy: "<br />")
+        let mondayLunchText = mondayLunch.joined(separator: "\n")
+        
+        print(mondayLunchText)
+        mondayFoodLabel.text = mondayLunchText
+        
+        //Tuesday
+        var startTue = htmlContentTue.range(of: "Tues,").location
+        htmlContentTue = htmlContentTue.substring(from: startTue) as NSString
+        startTue = htmlContentTue.range(of: "Soup:").location
+        htmlContentTue = htmlContentTue.substring(from: startTue) as NSString
+        let endTue = htmlContentTue.range(of: "</p>").location
+        let foodsForTuesday = htmlContentTue.substring(to: endTue)
+        let tuesdayLunch = foodsForTuesday.components(separatedBy: "<br />")
+        let tuesdayLunchText = tuesdayLunch.joined(separator: "\n")
+        
+        print(tuesdayLunchText)
+        tuesdayFoodLabel.text = tuesdayLunchText
+        
+        //Wednesday
+        var startWed = htmlContentWed.range(of: "Wed,&nbsp").location
+        htmlContentWed = htmlContentWed.substring(from: startWed) as NSString
+        startWed = htmlContentWed.range(of: "Soup:").location
+        htmlContentWed = htmlContentWed.substring(from: startWed) as NSString
+        let endWed = htmlContentWed.range(of: "</p>").location
+        let foodsForWednesday = htmlContentWed.substring(to: endWed)
+        let wednesdayLunch = foodsForWednesday.components(separatedBy: "<br />")
+        let wednesdayLunchText = wednesdayLunch.joined(separator: "\n")
+        
+        print(wednesdayLunchText)
+        wednesdayFoodlabel.text = wednesdayLunchText
+        
+        //Thursday
+        var startThu = htmlContentThu.range(of: "Thurs,").location
+        htmlContentThu = htmlContentThu.substring(from: startThu) as NSString
+        startThu = htmlContentThu.range(of: "Soup:").location
+        htmlContentThu = htmlContentThu.substring(from: startThu) as NSString
+        let endThu = htmlContentThu.range(of: "</td>").location
+        let foodsForThursday = htmlContentThu.substring(to: endThu)
+        let thursdayLunch = foodsForThursday.components(separatedBy: "<br />")
+        let thursdayLunchText = thursdayLunch.joined(separator: "\n")
+        
+        print(thursdayLunchText)
+        thursdayFoodLabel.text = thursdayLunchText
+        
+        //Friday
+        var startFri = htmlContentFri.range(of: "Fri,&nbsp").location
+        htmlContentFri = htmlContentFri.substring(from: startFri) as NSString
+        startFri = htmlContentFri.range(of: "Soup:").location
+        htmlContentFri = htmlContentFri.substring(from: startFri) as NSString
+        let endFri = htmlContentFri.range(of: "</td>").location
+        let foodsForFriday = htmlContentFri.substring(to: endFri)
+        let fridayLunch = foodsForFriday.components(separatedBy: "<br />")
+        let fridayLunchText = fridayLunch.joined(separator: "\n")
+        
+        print(fridayLunchText)
+        fridayFoodLabel.text = fridayLunchText
+        
+        
+        print(urlString)
   
     }
 
